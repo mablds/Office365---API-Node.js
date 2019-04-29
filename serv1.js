f2e399ab44a11b7ac39702f3d63786e7d1c66486
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-04-29 17:20:06 -0300

    _ working again

--- serv1.js
+++ serv1.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const aux = 0
 
 app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +15,11 @@ app.post('/op', (req, res) => {
     const operacao = req.body.op.split('+')
     const num1 = parseInt(operacao[0])
     const num2 = parseInt(operacao[1])
+    // solvedNumber += num1
+    // solvedNumber += num2
     const solvedNumber = num1 + num2
+    // aux += solvedNumber
     console.log(solvedNumber)
 })
+const bla = aux
+// module.exports.solvedNumber = bla
--- serv3.js
+++ serv3.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const aux = 0
+// const solvedNumber = 0
 
 app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +16,11 @@ app.post('/op', (req, res) => {
     const operacao = req.body.op.split('+')
     const num1 = parseInt(operacao[0])
     const num2 = parseInt(operacao[1])
+    // solvedNumber += num1
+    // solvedNumber += num2
     const solvedNumber = num1 + num2
+    // aux += solvedNumber
     console.log(solvedNumber)
 })
+const bla = aux
+// module.exports.solvedNumber = bla
